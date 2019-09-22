b151eecd21ea6f02952b6d1b7d0ca6fcb3bbeecf
Matthew Gleich <matthewgleich@gmail.com>
2019-09-22 00:20:37 -0400

    Update worker

--- worker/index.js
+++ worker/index.js
@@ -1,20 +1,19 @@
-const keys = require("./keys");
-const redis = require("redis");
+const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: key.redisHost,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
-function fib(index) {
-    if (index < 2) return 1;
-    return fib(index - 1) + fib(index - 2);
-}
-
 const sub = redisClient.duplicate();
 
-sub.on("message", (channel, message) => {
-    redisClient.hset("values", message. fib(parseInt(message)));
-})
-sub.subscribe("insert");
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
